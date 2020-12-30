d2c47077323076a347a1697b951606a3c2fbccee
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-12-30 06:18:11 +0100

    Refactored the add to current value

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 
 const scorePlayer1 = document.querySelector('#score--0');
 const scorePlayer2 = document.querySelector('#score--1');
-const currentPlayer1 = document.querySelector('#current--0');
-const currentPlayer2 = document.querySelector('#current--1');
+// const currentPlayer1 = document.querySelector('#current--0');
+// const currentPlayer2 = document.querySelector('#current--1');
 
 const dice = document.querySelector('.dice');
 
-const newButton = document.querySelector('.btn--new');
 const rollButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
-
-const player1Section = document.querySelector('.player--0');
-const player2Section = document.querySelector('.player--1');
+const newButton = document.querySelector('.btn--new');
 
 const WIN_VALUE = 100;
 
@@ -28,12 +25,15 @@ const changePlayer = (newPlayer, previousPlayer) => {
     .classList.remove('player--active');
 };
 
+const addToCurrent = (current, diceValue) => {
+  const currentPlayer = document.querySelector('#current--' + current);
+  currentPlayer.textContent = Number(currentPlayer.textContent) + diceValue;
+};
+
 let isPlayer1 = true;
 
 rollButton.addEventListener('click', () => {
   const diceValue = random();
-  const current1Text = currentPlayer1.textContent;
-  const current2Text = currentPlayer2.textContent;
 
   dice.src = `dice-${diceValue}.png`;
 
@@ -43,9 +43,5 @@ rollButton.addEventListener('click', () => {
     return;
   }
 
-  if (isPlayer1) {
-    currentPlayer1.textContent = Number(current1Text) + diceValue;
-  } else {
-    currentPlayer2.textContent = Number(current2Text) + diceValue;
-  }
+  isPlayer1 ? addToCurrent(0, diceValue) : addToCurrent(1, diceValue);
 });
