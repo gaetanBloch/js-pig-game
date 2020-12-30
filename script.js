042a91b888c07e57aaeb8e77243f88478b09a711
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-12-30 05:05:36 +0100

    Store all the needed DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const scorePlayer1 = document.querySelector('#score--0');
+const scorePlayer2 = document.querySelector('#score--1');
+const currentPlayer1 = document.querySelector('#current--0');
+const currentPlayer2 = document.querySelector('#current--1');
+
+const dice = document.querySelector('.dice');
+
+const newButton = document.querySelector('.btn--new');
+const rollButton = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
